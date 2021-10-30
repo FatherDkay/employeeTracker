9c206977c7b586068d9e70d16ecb7b2063047b4b
FatherDkay <paul.d.bailey@hotmail.com>
2021-10-30 08:51:47 -0700

    start of routes

--- server.js
+++ server.js
@@ -20,8 +20,82 @@ const db = mysql.createConnection(
     password: 'MyWifeLoves2Fuck',
     database: 'employeeTracker_db'
   },
-  console.log('Connected to the election database.')
+  console.log('Connected to the employeeTracker database.')
 );
+// Begin Queries
+// Employee Queries
+// Get all emplolyees
+app.get('/api/employee', (req, res) => {
+  const sql = `SELECT * FROM employee`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// Get a single employee
+app.get('/api/employee/:id', (req, res) => {
+  const sql = `SELECT * FROM employee WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+// Delete an employee
+app.delete('/api/employee/:id', (req, res) => {
+  const sql = `DELETE FROM employee WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Employee not found'
+      });
+    } else {
+      res.json({
+        message: 'Employee Deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+// Create an employee
+app.post('/api/employee', ({ body }, res) => {
+  const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+  VALUES (?,?,?,?)`;
+const params = [body.first_name, body.last_name, body.role_id, body.manager_id];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    res.status(400).json({ error: err.message });
+    return;
+  }
+  res.json({
+    message: 'Employee Created',
+    data: body
+  });
+});
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
